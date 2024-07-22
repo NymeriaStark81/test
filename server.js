e3183b8d1d6afcb8e769c965a73850408f6f5312
NymeriaStark81 <phantramy.nguyensieu@gmail.com>
2024-07-22 13:43:05 +0700

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const path = require('path')
 
 dotenv.config()
-if(process.env.NODE_EVN === 'local'){
+if(process.env.NODE_ENV === 'local'){
     app.use(cors({
         origin: 'http://localhost:5173/',
         credentials: true
@@ -17,7 +17,7 @@ if(process.env.NODE_EVN === 'local'){
 }
 
 
-if(process.env.NODE_EVN === 'production') {
+if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "./frontend/dist")))
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname, './', 'frontend', 'dist', 'index.html'))
@@ -26,7 +26,7 @@ if(process.env.NODE_EVN === 'production') {
 
 const dbConnect = async() => {
     try {
-        if(process.env.NODE_EVN === 'local'){
+        if(process.env.NODE_ENV === 'local'){
             await mongoose.connect(process.env.LOCAL_DB_URI)
             console.log('Database connecting ...')
         } else {
